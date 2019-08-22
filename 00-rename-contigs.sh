d3948b95dc2d043607e9fdb2336799483785b080
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2019-08-22 09:52:15 -0700

    initial commit

---
+++ 00-rename-contigs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source activate bbmap-env
+
+for item in *contigs.fasta; do 
+
+	outname=`basename $item .fasta`.renamed.fasta
+
+	prefix=`basename $outname .fasta`	
+
+	echo rename.sh in=$item out=$outname prefix=ESP_AE
+
+done
+
+conda deactivate
